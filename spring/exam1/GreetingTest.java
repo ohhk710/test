2f70c096f78e476b2724052610d94f4557d4684a
hyun <ohg147@naver.com>
2019-06-27 17:40:21 +0900

    greeting
    spring say morning greeting

---
+++ spring/exam1/GreetingTest.java
@@ -0,0 +1,29 @@
+package exam1;
+
+import java.time.LocalDateTime;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class GreetingTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext factory=
+				new ClassPathXmlApplicationContext("exam1/greetingBean.xml");
+		LocalDateTime local=(LocalDateTime)factory.getBean("timeBean");
+		
+		int hour = local.getHour();
+		
+		if(hour>=6 && hour<12)
+			((MorningGreetingImpl)factory.getBean("morningBean")).greet();
+		else if(hour >= 12 && hour < 17)
+			((AfternoonGreetingImpl)factory.getBean("afternoonbean")).greet();
+		else if(hour >= 17 && hour < 22)
+			((EveningGreetingImpl)factory.getBean("eveningbean")).greet();
+		else((NightGreetingImpl)factory.getBean("nightbean")).greet();
+		
+		((ClassPathXmlApplicationContext)factory).close();
+	}
+
+}
---
+++ spring/exam1/NightGreetingImpl.java
@@ -0,0 +1,12 @@
+package exam1;
+
+public class NightGreetingImpl implements Greeting {
+
+	@Override
+	public void greet() {
+		// TODO Auto-generated method stub
+		System.out.println("안녕히 주무세요.");
+	}
+
+	
+}
