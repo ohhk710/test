9ee512d431a45b84b4ef3c752da99c9845e027a5
ohg147 <ohg147@naver.com>
2019-07-21 23:50:18 +0900

    deleteAccount

---
+++ WebProject/DeleteAccount/SignupVO.java
@@ -0,0 +1,68 @@
+package vo;
+
+public class SignupVO {
+	int u_id;
+	String passwd;
+	String u_name;
+	String email;
+	int age;
+	String created_at;
+	String privilege;
+	String gender;
+	
+	@Override
+	public String toString() {
+		return "SignupVO [u_id=" + u_id + ", passwd=" + passwd + ", u_name=" + u_name + ", email=" + email + ", age="
+				+ age + ", created_at=" + created_at + ", privilege=" + privilege + ", gender=" + gender + "]";
+	}
+	public int getU_id() {
+		return u_id;
+	}
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getU_name() {
+		return u_name;
+	}
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+	public String getPrivilege() {
+		return privilege;
+	}
+	public void setPrivilege(String privilege) {
+		this.privilege = privilege;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+}
