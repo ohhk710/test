2f3c344956d95c95dcfa4725b4f601d2549379fa
Hyun kyoung OH <ohg147@naver.com>
2019-09-04 11:42:08 +0900

    db 연동 R

--- R/db1.R
+++ R/db1.R
@@ -8,12 +8,15 @@ names(iris)<-c("SLENGTH","SWIDTH","PLENGTH","PWIDTH","SPECIES")
 drv<-JDBC("oracle.jdbc.driver.OracleDriver","C:/hk/ojdbc6.jar")
 conn<-dbConnect(drv,"jdbc:oracle:thin:@localhost:1521:xe","jdbctest","jdbctest")
 
+#테이블생성
 dbWriteTable(conn, "iris", iris)
-
+#불러오기기
 result<-dbReadTable(conn,"IRIS")
 
 ggplot(result,aes(x=SLENGTH,y=SWIDTH))+
   geom_point(aes(colour=SPECIES))
+ggsave("db1.jpg")
 
 ggplot(result,aes(x=PLENGTH,y=PWIDTH))+
   geom_point(aes(colour=SPECIES))
+ggsave("db2.jpg")
